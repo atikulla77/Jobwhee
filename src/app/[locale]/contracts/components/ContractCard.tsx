b5bda76c7e3b586b308406f0c524496855fbec5a
codeWithThunder <atikulla345@gmail.com>
2025-04-28 17:29:50 +0600

    update some code

--- src/app/[locale]/contracts/components/ContractCard.tsx
+++ src/app/[locale]/contracts/components/ContractCard.tsx
@@ -62,7 +62,7 @@ const ContractCard: React.FC<{ contract: ContractCardProps }> = ({
 				{contract.description}
 			</p>
 
-			<div className="absolute left-0 bottom-0 sm:h-auto h-[180px] lg:px-9 md:px-7 sm:px-6 px-5 lg:pb-9 md:pb-7 sm:pb-6 pb-5  w-full flex flex-col sm:flex-row sm:flex-wrap justify-between items-start sm:items-center xl:mt-[50px] mt-[30px] gap-4">
+			<div className="absolute left-0 bottom-0 sm:h-auto h-[180px] lg:px-9 md:px-7 sm:px-6 px-5 lg:pb-9 md:pb-7 sm:pb-6 pb-5  w-full flex flex-col sm:flex-row sm:flex-wrap justify-between items-start sm:items-center gap-4">
 				<div className="flex sm:flex-row flex-wrap items-center gap-3 sm:gap-4 text-black text-sm xl:text-base lg:text-[16px] font-normal">
 					<div className="text-gray-500">
 						{contract.startDate} - {contract.endDate}
