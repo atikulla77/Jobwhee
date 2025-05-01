ef6c94510ed7c6a0f423794943a14e373943e36b
codeWithThunder <atikulla345@gmail.com>
2025-05-02 03:46:44 +0600

    Add Contract Completed Time Line Section

--- src/shared/widgets/FinancialSummary/FinancialSummary.tsx
+++ src/shared/widgets/FinancialSummary/FinancialSummary.tsx
@@ -17,7 +17,7 @@ const FinancialSummary = ({ contract }: any) => {
         <div className="2xl:w-[213px] xl:w-[175px] md:w-[227px] w-full h-full flex md:flex-row flex-col md:justify-between justify-start items-center 2xl:pr-[30px] xl:pr-[18px] md:pr-[35px] pr-0 md:gap-0 gap-[14px]">
           <div className="md:w-[1px] w-[100%] md:h-[90%] h-[1px] bg-[#CBEC5E]"></div>
           <div className="md:w-auto w-full flex flex-col md:justify-center justify-start items-center md:text-center text-left xl:gap-[12px] gap-[14px] ">
-            <h1 className="md:text-[16px] text-[20px] md:w-auto w-full">Total Spend</h1>
+            <h1 className="2xl:text-[20px] md:text-[16px] text-[20px] md:w-auto w-full">Total Spend</h1>
             <p className="text-[16px] font-normal md:w-auto w-full">€ {contract?.totalSpend}</p>
           </div>
         </div>
